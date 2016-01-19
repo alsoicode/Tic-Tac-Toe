15e8bb3b786a4628d2026fdcb0951182db54ce0f
Brandon Taylor <alsoicode@gmail.com>
2016-01-19 09:30:01 -0500

    Game.js WIP
    Added determination of winner.
    Refactored "positions" in game.
    Added ai player object.
    Added tracking of remaining positions available.
    Removed testing icon code in HTML.

--- static/js/src/game.js
+++ static/js/src/game.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 var TicTacToe = (function($) {
     'use strict';
 
@@ -8,12 +10,39 @@ var TicTacToe = (function($) {
     var _reset = function() {
         TicTacToe.resetControls.hide();
         TicTacToe.newGameControls.show();
-        TicTacToe.availablePositions.html('').removeClass('selected');
+        TicTacToe.positions.html('').removeClass('selected');
         TicTacToe.player = {
-            positions: []
+            positions: [],
+            winner: false
         };
+        TicTacToe.ai = {
+            positions: [],
+            winner: false
+        };
+        TicTacToe.availablePositions = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+        TicTacToe.activePlayer = TicTacToe.player;
 
         _play();
+    };
+
+    var _toggleActivePlayer = function() {
+        TicTacToe.activePlayer = TicTacToe.activePlayer === TicTacToe.player ? TicTacToe.ai : TicTacToe.player;
+    };
+
+    var _checkWinner = function() {
+
+        // compare activePlayer positions to winning combinations
+        _.each(TicTacToe.winningCombinations, function(combo) {
+
+            console.log(TicTacToe.activePlayer.positions, combo);
+
+            // console.log(_.intersection(combo, TicTacToe.activePlayer.positions).length);
+
+            if (_.intersection(combo, TicTacToe.activePlayer.positions).length === 3) {
+                TicTacToe.activePlayer.winner = true;
+                alert('winner');
+            }
+        });
     };
 
     var _play = function() {
@@ -27,12 +56,27 @@ var TicTacToe = (function($) {
         });
 
         // select position
-        TicTacToe.availablePositions.on('click', function(e) {
+        TicTacToe.positions.on('click', function(e) {
             var target = $(this);
 
             // if player has chosen a side and the space is available, mark it
             if (TicTacToe.player.side !== undefined && !target.hasClass('selected')) {
+
+                // set position as selected and add icon
                 target.addClass('selected').html(TicTacToe.sides[TicTacToe.player.side]);
+
+                // get position selected
+                var selectedPosition = target.data('position')
+
+                // Remove position from availablePositions
+                TicTacToe.availablePositions = _.reject(TicTacToe.availablePositions, function(position) {
+                    return position === selectedPosition;
+                })
+
+                // add position to player's positions
+                TicTacToe.player.positions.push(selectedPosition);
+
+                _checkWinner();
 
                 // check for winner
             }
@@ -41,7 +85,18 @@ var TicTacToe = (function($) {
 
     var init = function() {
         // get available positions and clear the board
-        TicTacToe.availablePositions = $('#board td');
+        TicTacToe.positions = $('#board td');
+
+        TicTacToe.winningCombinations = [
+            // horizontal
+            [1, 2, 3], [4, 5, 6], [7, 8, 9],
+
+            // vertical
+            [1, 4, 7], [2, 5, 8], [3, 6, 9],
+
+            // diagonal
+            [1, 5, 9], [3, 5, 7]
+        ];
         TicTacToe.newGameControls = $('.new-game-controls');
         TicTacToe.resetControls = $('.reset-controls');
         TicTacToe.sides = {
